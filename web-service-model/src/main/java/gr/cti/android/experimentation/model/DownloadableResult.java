520568218ba46d20fe7ef419be9984459d4d57a2
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2016-03-21 13:01:32 +0200

    downloadable - annonymized results

---
+++ web-service-model/src/main/java/gr/cti/android/experimentation/model/DownloadableResult.java
@@ -0,0 +1,61 @@
+package gr.cti.android.experimentation.model;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * Created by amaxilatis on 21/3/2016.
+ */
+public class DownloadableResult implements Comparable<DownloadableResult>, Serializable {
+    private long date;
+    private double longitude;
+    private double latitude;
+    private Map<String, Object> results;
+
+    public DownloadableResult() {
+    }
+
+    public DownloadableResult(long date, double longitude, double latitude, Map<String, Object> results) {
+        this.date = date;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.results = results;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public Map<String, Object> getResults() {
+        return results;
+    }
+
+    public void setResults(Map<String, Object> results) {
+        this.results = results;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    @Override
+    public int compareTo(DownloadableResult o) {
+        return (int) (o.getDate() - date);
+    }
+}
